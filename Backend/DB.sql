91b3d8677c7f17f0781ac1c2aed1d23993ec31f8
haijaa <antkarl11@gmail.com>
2024-12-03 14:36:35 +0100

    Fixade första starten och filer för cypress samt strukturera hur jag vill ha min databas. Inga inlägg där ännu.

---
+++ Backend/DB.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE GraphicMemo;
+
+CREATE TABLE comics (
+    comicId SERIAL PRIMARY KEY,
+    comicTitle VARCHAR(50) NOT NULL,
+    comicDescription TEXT NOT NULL,
+    comicIssue INT NOT NULL,
+    comicCharacter VARCHAR(50) NOT NULL,
+    comicAuthor VARCHAR(50),
+    comicPublisher VARCHAR(50) NOT NULL,
+    comicReleased VARCHAR(50),
+    comicImageCover VARCHAR(250) NOT NULL,
+)
+
+CREATE TABLE review (
+    reviewId SERIAL PRIMARY KEY,
+    comicId INT REFERENCES comics(comicId) ON DELETE CASCADE,
+    reviewUser VARCHAR(50) NOT NULL UNIQUE,
+    reviewText text,
+    reviewRaiting INT,
+)
